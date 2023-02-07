e628cccd7268daaae932a8f61b814cfa3ad45241
hashmi-shihab <hashmicse@gmail.com>
2023-02-07 11:23:13 +0600

    db connection check

--- app/Console/Commands/InfiLoop.php
+++ app/Console/Commands/InfiLoop.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use mysqli;
 
 class InfiLoop extends Command
 {
@@ -28,9 +29,10 @@ class InfiLoop extends Command
      */
     public function handle()
     {
-        $testArr=['1','2','3','4','5','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','21','22','23','24','25','26','27','28','29','30'];
+        /*================this is for infinite loop check====================*/
+        //$testArr=['1','2','3','4','5','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','21','22','23','24','25','26','27','28','29','30'];
         //dd($testArr);
-        while (true) {
+        /*while (true) {
             foreach ($testArr as $k=>$v){
                 try {
                     if ($k==2 || $k==4 || $k==28){
@@ -40,6 +42,32 @@ class InfiLoop extends Command
                     DB::table('tests')->insert(['err_index'=>$k]);
                 }
             }
+        }*/
+
+        /*================this is for db connection check for tds====================*/
+        $servername = "brixlyvps4.bondstein.info";
+        $username = "singularly";
+        $password = '@#$singularly@#';
+        $dbname = "bondstei_vtsdata";
+
+// Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
+
+        $sql = "SELECT * FROM terminaldata LIMIT 1";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                var_dump($row);
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
     }
 }
